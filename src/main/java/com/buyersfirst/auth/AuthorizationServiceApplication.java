2db15a19ae08f6c2d3660cb05cad060dfadcd467
Yoseph <yosephten@gmail.com>
2023-12-25 23:04:59 +0300

    `fix`: stoping spring secuirty

--- src/main/java/com/buyersfirst/auth/AuthorizationServiceApplication.java
+++ src/main/java/com/buyersfirst/auth/AuthorizationServiceApplication.java
@@ -2,8 +2,9 @@ package com.buyersfirst.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) /* To stop spring security from taking over */
 public class AuthorizationServiceApplication {
 
 	public static void main(String[] args) {
