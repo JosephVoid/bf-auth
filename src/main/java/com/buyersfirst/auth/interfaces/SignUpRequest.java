ea89c9b77f1086ba337167882ba99fca9620760c
Yoseph <yosephten@gmail.com>
2024-01-06 23:10:25 +0300

    `feat`: created signup interface

---
+++ src/main/java/com/buyersfirst/auth/interfaces/SignUpRequest.java
@@ -0,0 +1,11 @@
+package com.buyersfirst.auth.interfaces;
+
+public class SignUpRequest {
+    public String firstname;
+    public String lastname;
+    public String email;
+    public String phone;
+    public String password;
+    public String picture;
+    public String description;
+}
