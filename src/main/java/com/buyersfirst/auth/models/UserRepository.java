527424ed50bc36f9a227c40411cd4cbb0440df3a
Yoseph <yosephten@gmail.com>
2024-01-06 23:11:12 +0300

    `feat`: added new queries for db access

--- src/main/java/com/buyersfirst/auth/models/UserRepository.java
+++ src/main/java/com/buyersfirst/auth/models/UserRepository.java
@@ -1,7 +1,11 @@
 package com.buyersfirst.auth.models;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<Users, Integer>{
     Users findByEmail(String email);
+
+    List<Users> findUsersByEmail(String email);
 }
