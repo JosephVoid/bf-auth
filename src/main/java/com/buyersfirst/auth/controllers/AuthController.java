a0c5e85595006d779f32470667823866f49b335a
Yoseph <yosephten@gmail.com>
2024-02-08 18:26:13 +0300

    `feat`: added email validation

--- src/main/java/com/buyersfirst/auth/controllers/AuthController.java
+++ src/main/java/com/buyersfirst/auth/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.buyersfirst.auth.controllers;
 
 import java.sql.Timestamp;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,7 +32,7 @@ public class AuthController {
     JWTBuilder jwtBuilder;
 
     @PostMapping("/")
-    public @ResponseBody String getToken (@RequestBody TokenRequest auth) {
+    public @ResponseBody String getToken(@RequestBody TokenRequest auth) {
         try {
             Users user = userRepository.findByEmail(auth.email);
             BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10);
@@ -38,8 +40,7 @@ public class AuthController {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Email or password incorrect");
             if (bCryptPasswordEncoder.matches(auth.password, user.getPassword())) {
                 return jwtBuilder.generateToken(Integer.toString(user.getId()), "USER");
-            } 
-            else {
+            } else {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Email or password incorrect");
             }
         } catch (PersistenceException exception) {
@@ -48,25 +49,26 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public @ResponseBody String signUp (@RequestBody SignUpRequest request) {
+    public @ResponseBody String signUp(@RequestBody SignUpRequest request) {
         try {
             if (request.firstname == null || request.email == null || request.password == null || request.phone == null)
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad Input");
+            if (!validateEmail(request.email))
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Email");
             if (userRepository.findUsersByEmail(request.email).size() > 0)
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email taken");
-            
+
             BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10);
-            
+
             Users user = new Users(
-                request.firstname, 
-                request.lastname, 
-                request.email, 
-                bCryptPasswordEncoder.encode(request.password), 
-                request.picture, 
-                request.description, 
-                request.phone, 
-                new Timestamp(System.currentTimeMillis())
-            );
+                    request.firstname,
+                    request.lastname,
+                    request.email,
+                    bCryptPasswordEncoder.encode(request.password),
+                    request.picture,
+                    request.description,
+                    request.phone,
+                    new Timestamp(System.currentTimeMillis()));
 
             userRepository.save(user);
 
@@ -76,4 +78,10 @@ public class AuthController {
         }
     }
 
+    public boolean validateEmail(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 }
